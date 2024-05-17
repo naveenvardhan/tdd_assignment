bece129c22a01f75ca70ce21b100d22908dbd325
Naveen Vardhan <naveenvardhan@blubirch.com>
2024-05-17 16:13:45 +0530

    support different delimiters, handling exceptions

--- string_calc_spec.rb
+++ string_calc_spec.rb
@@ -7,5 +7,20 @@ RSpec.describe StringCalculator do
       expect(object.add_numbers("1,5")).to eq(6)
     end
   end
+
+  context 'when the string has different delimiters and special characters' do
+    it 'returns sum of non negative numbers' do
+      object = StringCalculator.new
+      expect(object.add_numbers("//;\n1;2")).to eq(3)
+    end
+  end
+
+  context 'when the string has negative numbers' do
+    it 'raises exception' do
+      object = StringCalculator.new
+      expect(object.add_numbers("1,5,4,-1,-2")).to eq("Negative numbers are not allowed -1,-2")
+    end
+  end
+
 end
 
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,10 @@
 class StringCalculator
   
   def add_numbers(string_numbers)
+    string_numbers = string_numbers.sub("\n", ",")
     nums = string_numbers.split(',').map(&:to_i)
+    negative_nums = nums.select{|i| i < 0 }
+    raise "Negative numbers are not allowed #{negative_nums.join(',')}" if negative_nums.any?
     sum = nums.sum
     sum
   end
@@ -12,3 +15,13 @@ object = StringCalculator.new
 puts object.add_numbers("")
 puts object.add_numbers("1")
 puts object.add_numbers("1,5")
+# allowing multiple numbers
+puts object.add_numbers("1,5,4,6")
+# handling new lines
+puts object.add_numbers("1\n2,3")
+
+# support different delimiters
+puts object.add_numbers("//;\n1;2")
+
+# negative numbers
+puts object.add_numbers("1,5,4,-1,-2")
