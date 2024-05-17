f6decebad8cee2d516008cc99106ea8fff64c981
Naveen Vardhan <naveenvardhan@blubirch.com>
2024-05-17 16:31:09 +0530

    Allowing all test cases

--- string_calc_spec.rb
+++ string_calc_spec.rb
@@ -2,14 +2,14 @@ require 'rspec'
 
 RSpec.describe StringCalculator do
   describe "#add_numbers" do
-    it "returns sum of non negative numbers" do
+    it "returns sum of numbers" do
       object = StringCalculator.new
       expect(object.add_numbers("1,5")).to eq(6)
     end
   end
 
   context 'when the string has different delimiters and special characters' do
-    it 'returns sum of non negative numbers' do
+    it 'returns sum of numbers' do
       object = StringCalculator.new
       expect(object.add_numbers("//;\n1;2")).to eq(3)
     end
@@ -19,6 +19,20 @@ RSpec.describe StringCalculator do
     it 'raises exception' do
       object = StringCalculator.new
       expect(object.add_numbers("1,5,4,-1,-2")).to eq("Negative numbers are not allowed -1,-2")
+    end
+  end
+
+  context 'when the string has numbers greater than 1000' do
+    it 'ignores more than 1000' do
+      object = StringCalculator.new
+      expect(object.add_numbers("//;\n3;20,1002,2340")).to eq(23)
+    end
+  end
+
+  context 'when the string has delimiters can be of any length' do
+    it 'returns sum' do
+      object = StringCalculator.new
+      expect(object.add_numbers("//[***]\n1***2***3")).to eq(6)
     end
   end
 
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,10 +1,12 @@
 class StringCalculator
   
   def add_numbers(string_numbers)
-    string_numbers = string_numbers.sub("\n", ",")
+    string_numbers = string_numbers.gsub(/[!@%&;\n*]/, ',')
     nums = string_numbers.split(',').map(&:to_i)
     negative_nums = nums.select{|i| i < 0 }
     raise "Negative numbers are not allowed #{negative_nums.join(',')}" if negative_nums.any?
+
+    nums = nums.select{|i| i <= 1000}
     sum = nums.sum
     sum
   end
@@ -22,6 +24,15 @@ puts object.add_numbers("1\n2,3")
 
 # support different delimiters
 puts object.add_numbers("//;\n1;2")
+
+# ignoring more than 1000
+puts object.add_numbers("//;\n3;20,1002,2340")
+
+# Delimiters can be of any length
+puts object.add_numbers("//[***]\n1***2***3")
+
+# Allow multiple delimiters
+puts object.add_numbers("//[*][%]\n1*2%3")
 
 # negative numbers
 puts object.add_numbers("1,5,4,-1,-2")
